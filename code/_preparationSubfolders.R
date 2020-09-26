5b9fd518aab67388c3fff01b2e4b2ddd0d8ed612
LearnUseZone <70752474+LearnUseZone@users.noreply.github.com>
2020-09-26 14:22:17 +0200

    changes in 2 functions to not set "analysis" directory as the working directory

--- code/_preparationSubfolders.R
+++ code/_preparationSubfolders.R
@@ -31,14 +31,20 @@
 # generate temporary .Rmd files from original .Rmd files (saved in subfolders) into folder "analysis"
 generate_rmd <- function(path, alias, dir) {
   path <- base::paste0(dir, '/', path)
+
+
   abs.path <- tools::file_path_as_absolute(paste0('./', path))  # path to an original .Rmd file (this file will be rendered to .html file inside function wflow_build_dir())
+  rel.path <- file.path("analysis", path)
+
+
+
   setwd("./analysis")                                           # folder where base::cat() will save generated temporary .Rmd files; it has to be right before base::cat() function
   base::cat(
     "---\n",
     yaml::as.yaml(rmarkdown::yaml_front_matter(abs.path)),      # YAML header from an original .Rmd file
     "---\n\n",
     # "**Source file\\:** ", path, "\n\n",                      # link to original .Rmd file; update (it's commented) of issues/95
-    "```{r child = '", abs.path, "'}\n```",                     # r chunk code (not YAML header) containing absolute path to an original .Rmd file
+    "```{r child = '", abs.path, "'}\n```",            # https://github.com/jdblischak/workflowr/issues/111          # r chunk code (not YAML header) containing absolute path to an original .Rmd file
     file = alias,                                               # a name of file that will be created
     sep = "",
     append = F                                                  # overwrite a content of a file
@@ -89,7 +95,7 @@ wflow_build_dir()
 #     because critical is to have perfectly organized .Rmd files rather than .html files and let workflowr to take care of these .html files.
 #   - keep in mind to use correct hyperlinks to future .html files (this shouldn't be a problem)
 
-print("stop")
+print("stop")  # temporary for testing
 
 # step 7 - commit/publish, push
 workflowr::wflow_publish(".", "changes in 2 functions to not set \"analysis\" directory as the working directory")
