c08da32ed68c933fba76563aa7f3c14fc2a584fe
LearnUseZone <70752474+LearnUseZone@users.noreply.github.com>
2020-10-04 18:50:22 +0200

    added: files for package

---
+++ code/_workflowrsubdirsUDP_steps.R
@@ -0,0 +1,27 @@
+# steps #
+
+# step 1
+base::setwd("D:\\Cloud\\Sync\\Study\\Programming\\R\\Work\\Projects\\GitHub\\workflowrSubfolders")
+devtools::create("code/workflowrsubdirsUDP")
+# Following message appears:
+#   New project 'workflowrsubdirsUDP' is nested inside an existing project 'code/', which is rarely a good idea.
+#   If this is unexpected, the here package has a function, `here::dr_here()` that reveals why 'code/' is regarded as a project.
+#   Do you want to create anyway?
+
+#   1: Negative
+#   2: For sure
+#   3: No way
+
+#   Selection: 2
+
+# step 2
+# Close (click on the red x button in top right corner) a newly opened RStudio window.
+# Create new .R files in directory "code/workflowrsubdirsUDP/R": generate.rmd.R, generate.subdir.R
+# Create 1 function with 1 description per 1 .R file into these new .R files.
+
+# I'm currently creating those functions and descriptions???
+
+
+# used literature
+# [lit 1] https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
+# [lit 2] D:\Cloud\Sync\Study\Programming\R\Work\Projects\LearnUse R packages\steps to create and install my package.R
