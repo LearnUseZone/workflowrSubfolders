af632d77b95efe56e337704bab039edec8e1f54e
LearnUseZone <70752474+LearnUseZone@users.noreply.github.com>
2020-10-05 13:44:56 +0200

    updated: description files

--- code/_workflowrsubdirs_steps.R
+++ code/_workflowrsubdirs_steps.R
@@ -1,6 +1,7 @@
 # steps #
 
 # step 1
+available::available("workflowrsubdirss")  # [lit 3a]
 base::setwd("D:\\Cloud\\Sync\\Study\\Programming\\R\\Work\\Projects\\GitHub\\workflowrSubfolders")
 devtools::create("code/workflowrsubdirs")
 # Following message appears (shown options for Selection may differ each time):
@@ -16,6 +17,7 @@ devtools::create("code/workflowrsubdirs")
 
 # step 2
 # Close (click on the red x button in top right corner) a newly opened RStudio window.
+# Update file "workflowrsubdirs/DESCRIPTION" [lit 3b]
 # Create new .R files in directory "code/workflowrsubdirsUDP/R": generate.rmd.R, generate.subdir.R
 # Create 1 function with 1 description per 1 .R file into these new .R files.
 
@@ -42,3 +44,5 @@ workflowrsubdirs::generate.rmd()
 # used literature
 # [lit 1] https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
 # [lit 2] D:\Cloud\Sync\Study\Programming\R\Work\Projects\LearnUse R packages\steps to create and install my package.R
+# [lit 3a] https://www.youtube.com/watch?v=79s3z0gIuFU - time 00:52 - 02:08
+# [lit 3b] https://www.youtube.com/watch?v=79s3z0gIuFU - time 02:59 - 04:19
